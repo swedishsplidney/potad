5eb7809b63e40eb1e55929790fd8149efd9613cc
swedishsplidney <kipnhackett@gmail.com>
2026-09-14 20:39:04 -0600

    networking init

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,11 @@
 #include <sys/reboot.h>
 #include <cstdlib>
 #include <signal.h>
+#include <net/if.h>
+#include <sys/socket.h>
+#include <cstring>
+
+static volatile sig_atomic_t g_shutdown_cmd = 0;
 
 void mount_fs(const char* source, const char* target, const char* type, unsigned long flags) {
     mkdir(target, 0755);
@@ -23,6 +28,43 @@ void mount_fs(const char* source, const char* target, const char* type, unsigned
 
 void reap_zombies(int sig) {
     while (waitpid(-1, nullptr, WNOHANG) > 0) {}
+}
+
+void handle_shutdown_signal(int sig) {
+    if (sig == SIGUSR1) {
+        g_shutdown_cmd = RB_POWER_OFF;
+    } else if (sig == SIGTERM || sig == SIGINT) {
+        g_shutdown_cmd = RB_AUTOBOOT;
+    }
+}
+
+void setup_loopback() {
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) return;
+
+    struct ifreq ifr{};
+    strncpy(ifr.ifr_name, "lo", IFNAMSIZ - 1);
+    if (ioctl(sock, SIOCGIFFLAGS, &ifr) == 0) {
+        ifr.ifr_flags |= IFF_UP | IFF_RUNNING;
+        if (ioctl(sock, SIOCSIFFLAGS, &ifr) == 0) {
+            std::cout << "[potad init] brought up lo network interface\n";
+        }
+    }
+    close(sock);
+}
+
+void run_script(const char* path) {
+    if (access(path, X_OK) == 0) {
+        std::cout << "[potad init] running " << path << "...\n";
+        pid_t pid = fork();
+        if (pid == 0) {
+            char* const args[] = {(char*)path, nullptr};
+            execv(path, args);
+            _exit(1);
+        } else if (pid > 0) {
+            waitpid(pid, nullptr, 0);
+        }
+    }
 }
 
 int main() {
@@ -47,38 +89,51 @@ dP
     mount_fs("devtmpfs", "/dev", "devtmpfs", 0);
     mount_fs("devpts", "/dev/pts", "devpts", 0);
     mount_fs("tmpfs", "/tmp", "tmpfs", 0);
+    mount_fs("tmpfs", "/run", "tmpfs", 0);
 
     sethostname("potad", 5);
 
-    // sigchild ykyk
-    struct sigaction sa{};
-    sa.sa_handler = reap_zombies;
-    sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
-    sigaction(SIGCHLD, &sa, nullptr);
+    struct sigaction sa_chld{};
+    sa_chld.sa_handler = reap_zombies;
+    sa_chld.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+    sigaction(SIGCHLD, &sa_chld, nullptr);
 
-    // launch shell
-    pid_t pid = fork();
-    if (pid == 0) {
-        setsid();
-        int fd = open("/dev/console", O_RDWR);
-        if (fd >= 0) {
-            dup2(fd, 0);
-            dup2(fd, 1);
-            dup2(fd, 2);
-            ioctl(fd, TIOCSCTTY, 0);
-            if (fd > 2) close(fd);
+    struct sigaction sa_shut{};
+    sa_shut.sa_handler = handle_shutdown_signal;
+    sigaction(SIGUSR1, &sa_shut, nullptr);
+    sigaction(SIGTERM, &sa_shut, nullptr);
+
+    // init networking
+    setup_loopback();
+    run_script("/etc/rc.local");
+
+    // shell supervising
+    while (g_shutdown_cmd == 0) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            setsid();
+            int fd = open("/dev/console", O_RDWR);
+            if (fd >= 0) {
+                dup2(fd, 0);
+                dup2(fd, 1);
+                dup2(fd, 2);
+                ioctl(fd, TIOCSCTTY, 0);
+                if (fd > 2) close(fd);
+            }
+
+            char* const args[] = {(char*)"/bin/sh", nullptr};
+            execv("/bin/sh", args);
+            _exit(1);
+        } else if (pid > 0) {
+            int status;
+            while (waitpid(pid, &status, 0) == -1 && errno == EINTR) {
+                if (g_shutdown_cmd != 0) break;
+            }
         }
-
-        char* const args[] = {(char*)"/bin/sh", nullptr};
-        execv("/bin/sh", args);
-        _exit(1);
-    } else if (pid > 0) {
-        int status;
-        while (waitpid(pid, &status, 0) == -1 && errno == EINTR) {}
-        std::cout << "[potad init] shell exited. Powering off...\n";
-        sync();
-        reboot(RB_POWER_OFF);
     }
 
+    std::cout << "[potad init] powering off system...\n";
+    sync();
+    reboot(g_shutdown_cmd ? g_shutdown_cmd : RB_POWER_OFF);
     return 0;
 }
